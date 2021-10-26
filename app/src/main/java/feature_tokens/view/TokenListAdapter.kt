f8e600fd6fe60a00e840d2bd094f99f2dd88ff9a
Banto Balazs <balazs.banto@agroninja.com>
2021-10-26 10:11:37 +0200

    Converted from smallest unit

--- app/src/main/java/feature_tokens/view/TokenListAdapter.kt
+++ app/src/main/java/feature_tokens/view/TokenListAdapter.kt
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.cryptobalances.R
 import feature_tokens.domain.ERC20Token
 import kotlinx.android.synthetic.main.row_layout.view.*
+import kotlin.math.pow
 
 class TokenListAdapter(private val tokenList: ArrayList<ERC20Token>) :
     RecyclerView.Adapter<TokenListAdapter.ViewHolder>() {
@@ -32,7 +33,7 @@ class TokenListAdapter(private val tokenList: ArrayList<ERC20Token>) :
         fun bind(token: ERC20Token, position: Int) {
             itemView.setBackgroundColor(if (token.balance > 0) Color.parseColor(GREEN_COLOR) else Color.parseColor(RED_COLOR))
             itemView.token_name.text = token.name
-            itemView.token_balance.text = token.balance.toString()
+            itemView.token_balance.text = "${token.balance * 10.toDouble().pow(-18)} ${token.name}"
         }
 
     }
